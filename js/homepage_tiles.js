cf6bbe0a79c2f5c8fd8f6337d0f3a24a4b3cf9a9
David Rothfarb <david@dr809.dfci.harvard.edu>
2019-09-19 16:21:16 -0400

    XS: fixed banner logos, mission statement, fixed js homepage tile resizing to only run if screen width <= 767

--- js/homepage_tiles.js
+++ js/homepage_tiles.js
@@ -3,19 +3,23 @@
  * Homepage tile height equalizer
  *
  */
-( ($) => {
+(function($) {
 
-  $(document).ready( () => {
-    const homepage_tiles = $(".homepage-tile");
+  $(document).ready( function() {
+    if(window.innerWidth <= 767) {
+      return false;
+    }
+
+    var homepage_tiles = $(".homepage-tile");
     if (homepage_tiles.length === 0) {
       return false;
     }
 
 // Loop through all homepage tiles, add all tile heights
 // to an array, return the largest height value
-    const getTallestTile = (homepage_tiles) => {
-      const tallest = [];
-      homepage_tiles.map( (i, tile) => {
+    var getTallestTile = function(homepage_tiles) {
+      var tallest = [];
+      homepage_tiles.map( function(i, tile) {
         tallest.push(tile.offsetHeight);
       });
       return tallest.sort().pop();
@@ -23,8 +27,8 @@
 
 // Get the largest tile height value, then assign it to 
 // all homepage tiles
-    const tile_height = getTallestTile(homepage_tiles);
-    homepage_tiles.map( (i, tile) => {
+    var tile_height = getTallestTile(homepage_tiles);
+    homepage_tiles.map( function(i, tile) {
       tile.style.height = tile_height + "px";
     });
   });
